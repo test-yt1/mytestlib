2976fd82a2495ead36ac6380359f2a0232312890
test-yt1 <78023890+test-yt1@users.noreply.github.com>
2021-01-26 20:41:56 +0900

    Add files via upload

---
+++ mytestlib/greet.py
@@ -0,0 +1,3 @@
+def hello():
+    print('Hello My Test Library!')
+    
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-pyhello',
+    version='1.0.0',
+    url='https://pypi.org/manage/projects/',
+    author='yt',
+    author_email='l25lnn1111@gmail.com',
+    description='My Test Library',
+    packages='find_packages()'
+)
